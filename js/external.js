4c162d9881df19e9a28b3c926c66f28942dac048
Audiesha Brattin <b.audiesha@gmail.com>
2020-12-10 09:09:09 -0600

    adding 1st JS exercise

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log('Hello from JavaScript!');
+alert("Welcome To My Website!");
+let  userInput = prompt("What is your favorite color?");
+alert ("Awesome! I love " + userInput + "!" + " That is my favorite color too!");
+
+
+
