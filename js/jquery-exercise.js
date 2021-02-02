d0aeaa4a4c3b8ba3f703226e6f872c676fc99058
audiesha brattin <a.brattin@gmail.com>
2021-02-02 15:18:10 -0600

    selectors ex

--- js/jquery-exercise.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-$(document).ready(function() {
-alert("The dom has finished loading");
-});
---
+++ js/my-custom-javascript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('li').css('font-size', '20px');
+  $('p, h1, li').css('background-color', 'pink');
+});
+var contents = $('h1').html();
+alert(contents);
