2ee988cc33bb0891b56ea4ab7648b400cf24f5aa
audiesha brattin <a.brattin@gmail.com>
2021-02-03 11:09:24 -0600

    commit before switching branches

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,6 +1,45 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $('li').css('font-size', '20px');
   $('p, h1, li').css('background-color', 'pink');
 });
-var contents = $('h1').html();
-alert(contents);
+let contents = $('h1').html();
+alert(contents);*/
+
+/*Changes the h1 Background to Red*/
+$('h1').click(function () {
+  $(this).css({
+    backgroundColor: 'red',
+    transition: 'background .5s'
+  })
+});
+
+(function () {
+  let jqElements = $('p');
+  let fontReset = ''
+
+  function addClickEvents(elementsToBind) {
+    let changePFont = function () {
+      elementsToBind.css('font-size', fontReset);
+      $(this).css("font-size", '18px');
+    }
+    elementsToBind.dblclick(changePFont);
+  }
+
+  addClickEvents(jqElements);
+})();
+
+(function () {
+  let liObject = $('li');
+  let handlerRedBGnd = function () {
+    $(this).css({
+      backgroundColor: 'red',
+      transition: 'background .5s'
+    })
+  };
+  let handlerNormalBGnd = function (e) {
+    $(this).css('background-color', "black")
+  }
+
+  liObject.hover(handlerRedBGnd, handlerNormalBGnd);
+})();
+
