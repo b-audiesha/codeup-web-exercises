8c2977209515897411144b35b1467a527f232ee0
audiesha brattin <a.brattin@gmail.com>
2021-02-04 09:38:07 -0600

    adding

--- js/konami.js
+++ js/konami.js
@@ -1,23 +1,22 @@
-(function () {
-  'use strict';
-  $().ready(function () {
-    function konami(fn) {
-      let keysEntered = ""; /*empty string*/
-      let konamiCode = "38384040373937396665"; /*the keycode string*/
-      $(document).keyup(function (event) {
-        keysEntered += event.keyCode; /*pushing to the array*/
-        console.log(keysEntered); /*console log the keys up*/
-        /*   keysEntered.splice(-keysEntered - 1, keysEntered.length);*/ /*comparing it to a konamiCode string*/
-        if (keysEntered.indexOf(konamiCode) !== -1)/*if keys entered matches the konami code then alert*/{
-          fn();
-          keysEntered = ""
-        }
-      });//29
-    }
-    $(document).ready(function () {
-      konami(function () {
-        alert(" Huzzah You NOW have 30 extra lives!");
-      });
-    });//39
-  });//25
+'use strict';
+$(document).ready(function () {
+  function konami(fn) {
+    let keysEntered = ""; /*empty string*/
+    let konamiCode = "38384040373937396665"; /*the keycode string*/
+    $(document).keyup(function (event) {
+      keysEntered += event.keyCode; /*pushing to the array*/
+      if (keysEntered.indexOf(konamiCode) !== -1)/*if keys entered matches the konami code then alert*/{
+        fn();
+        keysEntered = ""
+      }
+    });//29
+  }
+  $(document).ready(function () {
+    konami(function () {
+      alert(" Huzzah You NOW have 30 extra lives!");
+      $('body').css('background-color', 'pink');
+    });
+  });//39
 })();//23
+
+/* everything in the head runs first and then everything after it renders.*/
