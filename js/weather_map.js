aaab79364cedd03142319983a264b6291bbc46bc
audiesha brattin <a.brattin@gmail.com>
2021-02-10 21:48:50 -0600

    adding table data

---
+++ js/weather_map.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  // button for search
+  $("submitForcast").click(function () {
+    return getForcast();
+  });
+
+});
+
+function getForcast() {
+  let city = $('#city').val();
+  let days = $('#days').val();
+// make sure that value is indeed a value and not empty else throw an error.
+//     Call Ajax
+//   if (city != '' && days != '') {
+
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/onecall?&=" + city + "&units=imperial" + "&cnt=" + days +
+        "&APPID=OPEN_WEATHER_APPID",
+      type: "GET",
+      dateType: "jsonp",
+      success: function (data) {
+        let table = ""
+        // start at 0 and loop thru all days (4)
+        for (let i = 0; i < data.list.length; i++) {
+          table += '<tr>';
+          table+= '<td>' + data.list[i].weather[0].icon + '</td>';
+          table+= '<td>' + data.list[i].weather[0].main + '</td>';
+          table+= '<td>' + data.list[i].weather[0].description + '</td>';
+          table += '</tr>';
+        }},})}
+$('#weatherContent').html();
+$('#city').val('');
+$('#days').val('');
